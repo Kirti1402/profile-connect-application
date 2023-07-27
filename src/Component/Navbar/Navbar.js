b8efce2e2314c32dfa7411f2d936598ad4e1ad8d
Kirti Singh <kirtisingh1497@gmail.com>
2023-07-27 23:30:29 +0530

    feat: created folder structure for Home Page

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Profile</li>
+        <li>Post</li>
+        <li>Gallery</li>
+        <li>To-Do</li>
+      </ul>
+    </div>
+  );
+};
