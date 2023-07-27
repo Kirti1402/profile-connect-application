2244f0db2ad5cce46cf0bb2b6fb3864260800a39
Kirti Singh <kirtisingh1497@gmail.com>
2023-07-28 03:06:30 +0530

    feat: destructured the user detail

--- src/Component/Profile/UserProfileDetail.js
+++ src/Component/Profile/UserProfileDetail.js
@@ -1,19 +1,29 @@
-import React,{useContext} from 'react'
-import { UserProfileContext } from '../../Context/UserProfileContext'
-
+import React, { useContext } from "react";
+import { UserProfileContext } from "../../Context/UserProfileContext";
 
 export const UserProfileDetail = () => {
-  const {userProfile} =useContext(UserProfileContext)
-  console.log(userProfile)
-  const {} = userProfile
+  const { userProfile } = useContext(UserProfileContext);
+  console.log(userProfile);
+  const {
+    id,
+    name,
+    username,
+    email,
+    profilepicture,
+    address: {
+      city,
+      geo: { lat, lng },
+      street,
+      suite,
+      zipcode,
+    },
+    company: { name: companyname, catchPhrase, bs },
+    website
+  } = userProfile;
+  
   return (
     <div>
-      
-        <div>
-
-          </div>
-        
-      
+      <div></div>
     </div>
-  )
-}
+  );
+};
